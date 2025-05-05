0df8bf0029beea8a56aced242f4926f4b8d18a6d
sarthakdarade07 <sarthakdarade07@gmail.com>
2025-05-06 01:21:44 +0530

    added 1 to 15

--- 13_sum_naturalNos.cpp
+++ 13_sum_naturalNos.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Write a Program to Find the Sum of the First N Natural Numbers
 
 #include <iostream>
@@ -19,25 +18,3 @@ int main()
     cout << "sum is: " << sum;
     return 0;
 }
-=======
-// Write a Program to Find the Sum of the First N Natural Numbers
-
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int sum = 0, n;
-
-    cout << "Enter upto which you want sum:";
-    cin >> n;
-
-    for (int i = 1; i <= n; i++)
-    {
-        sum += i;
-    }
-
-    cout << "sum is: " << sum;
-    return 0;
-}
->>>>>>> ab7935dc1c79becf354efcedebcdc9960dd8ec9c
--- 15_check_leap_year.cpp
+++ 15_check_leap_year.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Write a Program to Find a Leap Year or Not
 
 #include<iostream>
@@ -38,44 +37,3 @@ bool check_leap_year(int year){
         return false;
     }
 }
-=======
-// Write a Program to Find a Leap Year or Not
-
-#include<iostream>
-using namespace std;
-
-bool check_leap_year(int);
-
-int main(){
-    int year;
-
-    cout<<"Enter Year:";
-    cin>>year;
-
-    if(check_leap_year(year)){
-        cout<<year<<" is leap year";
-    }
-    else{
-        cout << year << " is not leap year";
-    }
-
-    return 0;
-}
-
-bool check_leap_year(int year){
-    
-    if(year%400==0){
-        return true;
-    }
-      if(year%100==0){
-        return false;
-    }
-
-     if(year%4==0){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
->>>>>>> ab7935dc1c79becf354efcedebcdc9960dd8ec9c
